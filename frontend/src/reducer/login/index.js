0bcd5c1b771fe7387bd92c6f21ea16d46bd3e52e
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-18 14:06:38 +0300

    create login reducer

---
+++ frontend/src/reducer/login/index.js
@@ -0,0 +1,18 @@
+const initialState = {
+    token:"",
+  };
+
+
+
+  const token = (state = initialState, { type, payload }) => {
+    switch (type) {
+      case 'SET_TODOS':
+        return { token : payload };
+  
+    }};
+  
+    
+  export const setToken = (token) => {
+    return { type: "SET_TOKEN" ,
+     payload : token }
+  };
